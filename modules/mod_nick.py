2e1f3e784e1563fb41b428d267169ecc182e95c4
Chiruclan <administrator@chiruclan.de>
2013-01-30 18:06:11 +0100

    [935] hope it works now - reserved modules folder for commands and user defined modules
    Signed-off-by: Chiruclan <administrator@chiruclan.de>

--- modules/mod_nick.py
+++
@@ -1,7 +0,0 @@
-from chiruserv import CServMod
-
-class mod_nick(CServMod):
-	MODULE_CLASS = "NICK"
-	
-	def onData(self, data):
-		self.query("update online set nick = ? where uid = ?", data.split()[2], str(data.split()[0])[1:])
--- modules/mod_opertype.py
+++
@@ -1,8 +0,0 @@
-from chiruserv import CServMod
-
-class mod_opertype(CServMod):
-	MODULE_CLASS = "OPERTYPE"
-	
-	def onData(self, data):
-		uid = data.split()[0][1:]
-		self.query("insert into opers values (?)", uid)
