027bf17a604ab67e1c233475322238c06e11e23f
Chiruclan <administrator@chiruclan.de>
2013-01-30 19:29:56 +0100

    [937] just one last try :)
    Signed-off-by: Chiruclan <administrator@chiruclan.de>

---
+++ modules/mod_quit.py
@@ -0,0 +1,17 @@
+from chiruserv import CServMod
+
+class mod_quit(CServMod):
+	MODULE_CLASS = "QUIT"
+	
+	def onData(self, data):
+		for qchan in self.query("select * from chanlist where uid = ?", data.split()[0][1:]):
+			if self.chanflag("l", qchan["channel"]):
+				if len(data.split()) == 2:
+					self.log(qchan["uid"], "quit", qchan["channel"])
+				else:
+					self.log(qchan["uid"], "quit", qchan["channel"], ' '.join(data.split()[2:])[1:])
+					
+		self.query("delete from chanlist where uid = ?", data.split()[0][1:])
+		self.query("delete from temp_nick where nick = ?", str(data.split()[0])[1:])
+		self.query("delete from gateway where uid = ?", str(data.split()[0])[1:])
+		self.query("delete from online where uid = ?", str(data.split()[0])[1:])
