04b61b3bc89bbd539006678bdc93656d46eebec4
Chiruclan <administrator@chiruclan.de>
2013-01-31 16:01:54 +0100

    [949] adding hostinfo command
    Signed-off-by: Chiruclan <administrator@chiruclan.de>

---
+++ modules/cmd_hostinfo.py
@@ -0,0 +1,26 @@
+from chiruserv import CServMod
+from socket import getfqdn, getaddrinfo
+
+class cmd_hostinfo(CServMod):
+	COMMAND = "HOSTINFO"
+	HELP = "Get information about a domain or IP"
+	NEED_OPER = 1
+	
+	def onCommand(self, uid, args):
+		msg(nick, "-=- Get Domain/IP information -=-")
+		msg(nick)
+		msg(nick, "Host: " + getfqdn(args))
+		
+		ip = "0.0.0.0"
+		
+		for data in getaddrinfo(args, None):
+			if ip != data[4][0]:
+				ip = data[4][0]
+				
+				if ip.find(":") != -1:
+					msg(nick, "IPv6: " + ip)
+				else:
+					msg(nick, "IP: " + ip)
+					
+		msg(nick)
+		msg(nick, "-=- End of Domain/IP information -=-")
