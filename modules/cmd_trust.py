b410630719d5198f86d444f646a5dd40225758b2
Chiruclan <development@chiruclan.de>
2013-03-03 12:41:21 +0100

    [1039] Dev/Commit: changing limit on update :)
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/cmd_trust.py
+++ modules/cmd_trust.py
@@ -53,31 +53,6 @@ class cmd_trust(CServMod):
 				self.msg(uid, "Done.")
 			else:
 				self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
-		elif len(arg) == 3:
-			if arg[0].lower() == "update":
-				if arg[2].isdigit():
-					tuid = arg[1]
-					ttime = int(arg[2])
-					
-					if tuid != "0.0.0.0" and (wmatch(tuid, "*.*") or wmatch(tuid, "*:*")):
-						for row in self.query("SELECT `id` FROM `trust` WHERE `address` = ?", tuid):
-							etime = int(time.time()) + int(ttime * 60 * 60 * 24)
-							self.query("UPDATE `trust` SET `timestamp` = ? WHERE `address` = ?", etime, tuid)
-							
-							for row in self.query("SELECT `uid` FROM `online` WHERE `address` = ? OR `host` = ?", tlimit, tlimit):
-								self.checkconnection(row["uid"])
-								
-							self.msg(uid, "Done.")
-							self.send_to_op("#Trust# " + tuid + " updated (Time left: " + self.convert_timestamp(int(ttime * 60 * 60 * 24)) + ")")
-							return 0
-							
-						self.msg("Failed. There is no such record.")
-					else:
-						self.msg(uid, "Denied.")
-				else:
-					self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
-			else:
-				self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
 		elif len(arg) == 4:
 			if arg[0].lower() == "set":
 				if arg[2].isdigit() and arg[3].isdigit():
@@ -102,6 +77,31 @@ class cmd_trust(CServMod):
 						self.msg(uid, "Denied.")
 				else:
 					self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
+			elif arg[0].lower() == "update":
+				if arg[2].isdigit() and arg[3].isdigit():
+					tuid = arg[1]
+					ttime = int(arg[2])
+					tlimit = int(arg[3])
+					
+					if tuid != "0.0.0.0" and (wmatch(tuid, "*.*") or wmatch(tuid, "*:*")):
+						for row in self.query("SELECT `id` FROM `trust` WHERE `address` = ?", tuid):
+							etime = int(time.time()) + int(ttime * 60 * 60 * 24)
+							self.query("UPDATE `trust` SET `timestamp` = ?, `limit` = ? WHERE `id` = ?", etime, tlimit, row["id"])
+							
+							for row in self.query("SELECT `uid` FROM `online` WHERE `address` = ? OR `host` = ?", tuid, tuid):
+								self.checkconnection(row["uid"])
+								
+							self.msg(uid, "Done.")
+							self.send_to_op("#Trust# " + tuid + " updated (Time left: " + self.convert_timestamp(int(ttime * 60 * 60 * 24)) + ")")
+							return 0
+							
+						self.msg("Failed. There is no such record.")
+					else:
+						self.msg(uid, "Denied.")
+				else:
+					self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
+			else:
+				self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
 			else:
 				self.msg(uid, "Syntax: TRUST <set/update/del/list/search> [<address/id> [<time (in days)> [<limti>]]]")
 		else:
