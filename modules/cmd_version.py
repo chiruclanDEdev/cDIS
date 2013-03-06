863bb56d75aff14af01bd6fba27c7642d9028c13
Chiruclan <development@chiruclan.de>
2013-03-06 10:16:22 +0100

    [1078] Dev/List: 055eab359e6135480256c30f1957b983
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/cmd_version.py
+++ modules/cmd_version.py
@@ -12,10 +12,10 @@ class cmd_version(cDISModule):
 		file.close()
 		self.msg(uid, "<= chiruclan.de IRC services {0} =>".format(version))
 		self.msg(uid)
-		self.msg(uid, "  => Hash: {0}".format(Popen("git describe --match init --dirty=+ --abbrev=12 --tags", shell=True, stdout=PIPE).stdout.read().rstrip().split("-")[-1][1:]))
+		self.msg(uid, "  Hash: {0}".format(Popen("git describe --match init --dirty=+ --abbrev=12 --tags", shell=True, stdout=PIPE).stdout.read().rstrip().split("-")[-1][1:]))
 		
 		if self.isoper(uid):
-			self.msg(uid, "  => Last update: {0}".format(Popen("git show -s --format=%ci", shell=True, stdout=PIPE).stdout.read().rstrip()))
+			self.msg(uid, "  Last update: {0}".format(Popen("git show -s --format=%ci", shell=True, stdout=PIPE).stdout.read().rstrip()))
 
 		options = list()
 		
@@ -26,11 +26,11 @@ class cmd_version(cDISModule):
 			options.append("IPv6")
 				
 		if len(options) != 0:
-			self.msg(uid, "  => Options: {0}".format(', '.join(options)))
+			self.msg(uid, "  Options: {0}".format(', '.join(options)))
 			
 		if self.isoper(uid):
-			self.msg(uid, " => If you're looking for more modules, check this out: https://github.com/chiruclanDEdev/cDIS-Modules")
+			self.msg(uid, "  If you're looking for more modules, check this out: https://github.com/chiruclanDEdev/cDIS-Modules")
 			
-		self.msg(uid, " => Developed by chiruclan.de Development (https://github.com/chiruclanDEdev). Suggestions to hosting@chiruclan.de.")
+		self.msg(uid, "  Developed by chiruclan.de Development (https://github.com/chiruclanDEdev). Suggestions to hosting@chiruclan.de.")
 		self.msg(uid)
 		self.msg(uid, "<= Version end =>")
