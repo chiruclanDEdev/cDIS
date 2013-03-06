9e541b6b555d15df425aedcd2357c5a5e27021c9
Chiruclan <development@chiruclan.de>
2013-03-06 10:13:54 +0100

    [1075] Dev/List: 72163f61768abbd601a8de3f4181ee73
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/cmd_version.py
+++ modules/cmd_version.py
@@ -6,16 +6,16 @@ class cmd_version(cDISModule):
 	COMMAND = "VERSION"
 	HELP = "Shows version of services"
 
-	def onCommand(self, source, args):
+	def onCommand(self, uid, args):
 		file = open("version", "r")
 		version = file.read()
 		file.close()
 		self.msg(uid, "<= chiruclan.de IRC services {0} =>".format(version))
 		self.msg(uid)
-		self.msg(source, "  => Hash: {0}".format(Popen("git describe --match init --dirty=+ --abbrev=12 --tags", shell=True, stdout=PIPE).stdout.read().rstrip().split("-")[-1][1:]))
+		self.msg(uid, "  => Hash: {0}".format(Popen("git describe --match init --dirty=+ --abbrev=12 --tags", shell=True, stdout=PIPE).stdout.read().rstrip().split("-")[-1][1:]))
 		
-		if self.isoper(source):
-			self.msg(source, "  => Last update: {0}".format(Popen("git show -s --format=%ci", shell=True, stdout=PIPE).stdout.read().rstrip()))
+		if self.isoper(uid):
+			self.msg(uid, "  => Last update: {0}".format(Popen("git show -s --format=%ci", shell=True, stdout=PIPE).stdout.read().rstrip()))
 
 		options = list()
 		
@@ -26,11 +26,11 @@ class cmd_version(cDISModule):
 			options.append("IPv6")
 				
 		if len(options) != 0:
-			self.msg(source, "  => Options: {0}".format(', '.join(options)))
+			self.msg(uid, "  => Options: {0}".format(', '.join(options)))
 			
-		if self.isoper(source):
-			self.msg(source, " => If you're looking for more modules, check this out: https://github.com/chiruclanDEdev/cDIS-Modules")
+		if self.isoper(uid):
+			self.msg(uid, " => If you're looking for more modules, check this out: https://github.com/chiruclanDEdev/cDIS-Modules")
 			
-		self.msg(source, " => Developed by ChiruclanDE (https://github.com/chiruclanDEdev). Suggestions to hosting@chiruclan.de.")
+		self.msg(uid, " => Developed by chiruclan.de Development (https://github.com/chiruclanDEdev). Suggestions to hosting@chiruclan.de.")
 		self.msg(uid)
 		self.msg(uid, "<= Version end =>")
