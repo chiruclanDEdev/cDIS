17d263f1fb2831853f137d8791cda13b38daf4ae
Chiruclan <development@chiruclan.de>
2013-03-19 14:36:22 +0100

    [1083] Dev/List: f8f2a7a28fca6e6fb0c8e90f96fe3735
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/sched_trust.py
+++ modules/sched_trust.py
@@ -8,7 +8,7 @@ class sched_trust(cDISModule):
 	def onSchedule(self):
 		while True:
 			current_timestamp = int(time.time())
-			result = self.query("SELECT `id`, `address`, `limit`, FROM `trust` WHERE `timestamp` <= ?", current_timestamp)
+			result = self.query("SELECT `id`, `address`, `limit` FROM `trust` WHERE `timestamp` <= ?", current_timestamp)
 			for row in result:
 				self.query("DELETE FROM `trust` WHERE `id` = ?", row["id"])
 				data = self.query("SELECT `uid` FROM `online` WHERE `address` = ? OR `host` = ?", row["address"], row["address"])
