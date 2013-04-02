ef5c854fa851c76fbeedad5548f1f53bc84102ab
Chiruclan <development@chiruclan.de>
2013-04-02 14:25:12 +0200

    fixed help
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/cmd_2_help.py
+++ modules/cmd_2_help.py
@@ -10,7 +10,7 @@ class cmd_2_help(cDISModule):
 		self.msg(source, "The following commands are available to you.")
 		
 		if len(args) == 0:
-			for command in self.query("SELECT * FROM `modules` WHERE `class` = 'COMMAND' AND `bot` = ? ORDER BY `command`", BOT_ID):
+			for command in self.query("SELECT * FROM `modules` WHERE `class` = 'COMMAND' AND `bot` = ? ORDER BY `command`", self.BOT_ID):
 				if os.access("modules/"+command["name"]+".py", os.F_OK):
 					cmd_auth = command["auth"]
 					cmd_help = command["help"]
@@ -20,7 +20,7 @@ class cmd_2_help(cDISModule):
 					elif cmd_auth and self.auth(source):
 						self.help(source, command["command"], cmd_help)
 		else:
-			for command in self.query("SELECT * FROM `modules` WHERE `class` = 'COMMAND' AND `bot` = ? AND `command` LIKE ?", '%' + args + '%'):
+			for command in self.query("SELECT * FROM `modules` WHERE `class` = 'COMMAND' AND `bot` = ? AND `command` LIKE ?", self.BOT_ID, '%' + args + '%'):
 				if os.access("modules/"+command["name"]+".py", os.F_OK):
 					cmd_auth = command["auth"]
 					cmd_help = command["help"]
