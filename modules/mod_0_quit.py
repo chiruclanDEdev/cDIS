619602c7d9d16c95ad155d2d8fecbb43c2a32075
Chiruclan <development@chiruclan.de>
2013-07-20 18:22:26 +0200

    [1167] Module/IRCOp: More security for IRC operator accounts that are saved in the MySQL database.
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/mod_0_quit.py
+++ modules/mod_0_quit.py
@@ -20,13 +20,18 @@ class mod_0_quit(cDISModule):
   MODULE_CLASS = "QUIT"
   
   def onData(self, data):
-    for qchan in self.query("select * from chanlist where uid = ?", data.split()[0][1:]):
+    uid = data.split()[0][1:]
+    
+    for qchan in self.query("select * from chanlist where uid = ?", uid):
       if self.chanflag("l", qchan["channel"]):
         if len(data.split()) == 2:
           self.log(qchan["uid"], "quit", qchan["channel"])
         else:
           self.log(qchan["uid"], "quit", qchan["channel"], ' '.join(data.split()[2:])[1:])
           
+    if self.isoper(uid):
+      self.query("UPDATE `ircd_opers` SET `hostname` = 'root@localhost' WHERE `hostname` = ?", self.userhost(uid))
+    
     self.query("delete from chanlist where uid = ?", data.split()[0][1:])
     self.query("delete from gateway where uid = ?", str(data.split()[0])[1:])
     self.query("delete from online where uid = ?", str(data.split()[0])[1:])
