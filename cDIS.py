36322408906984ae46a79283e96c0ac2fd666bd1
Chiruclan <development@chiruclan.de>
2013-12-07 16:19:22 +0100

    [1190] Core/Password: try to fix createPassword(key) function
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- cDIS.py
+++ cDIS.py
@@ -215,7 +215,8 @@ class Services:
 class cDISModule:
   import sys
   import time
-  import hashlib, hmac
+  import hashlib
+  import hmac
   import smtplib
   import psycopg2.extras
   import subprocess
@@ -917,10 +918,11 @@ class cDISModule:
   def encode_md5(self, string):
     return hashlib.md5(bytes(string, "UTF-8")).hexdigest()
     
-  def createPassword(self, key = time.time()):
+  def createPassword(self, password=None, key = time.time()):
     password = list()
     password.append(hmac.new(bytes(key, "UTF-8"), bytes(time.time(), "UTF-8"), hashlib.md5).hexdigest())
     password.append(self.encode(password[0]))
+    
     return password
 
   def mail(self, receiver, subject, message):
