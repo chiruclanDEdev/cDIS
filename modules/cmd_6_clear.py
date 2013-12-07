5682a886a0c289f5e7685890b11ec8281b4f249e
Chiruclan <development@chiruclan.de>
2013-12-07 22:31:22 +0100

    [1234] Dev/Commit: add clear command
    Signed-off-by: Chiruclan <development@chiruclan.de>

--- modules/cmd_6_clear.py
+++ modules/cmd_6_clear.py
@@ -16,28 +16,14 @@
 
 from cDIS import cDISModule
 
-class cmd_6_delete(cDISModule):
-  HELP = "Deletes a message"
+class cmd_6_clear(cDISModule):
+  HELP = "Deletes all messages in your inbox"
   NEED_AUTH = 1
   MODULE_CLASS = "COMMAND"
-  COMMAND = "DELETE"
+  COMMAND = "CLEAR"
   BOT_ID = '6'
   
   def onCommand(self, uid, args):
-    arg = args.split()
-    
-    if (len(arg) == 1):
-      if (arg[0].isnumeric()):
-        account = self.auth(uid)
-        id = int(arg[0])
-        count = self.query("""SELECT COUNT(*) FROM "memo" WHERE "recipient" = %s AND "id" = %s""", account, id)[0]["count"]
-        
-        if count == 1:
-          self.query("""DELETE FROM "memo" WHERE "recipient" = %s AND "id" = %s""", account, id)
-          self.msg(uid, "Done.")
-        else:
-          self.msg(uid, "Couldn't locate message #{0}.".format(id))
-      else:
-        self.msg(uid, "Please enter a valid number.")
-    else:
-      self.msg(uid, "Syntax: DELETE <ID>")
+    account = self.auth(uid)
+    self.query("""DELETE FROM "memo" WHERE "recipient" = %s""", account)
+    self.msg(uid, "Done.")
